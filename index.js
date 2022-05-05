153de10d64ad36fc4ec06c171ecd82731b58c0fa
fchristovam <fchristovam7@gmail.com>
2022-05-05 17:44:24 -0300

    improve my sprint

--- index.js
+++ index.js
@@ -6,16 +6,42 @@ var codifica = document.getElementById('cod');
 var decodifica = document.getElementById('decod');
 var botao = document.getElementById('botao');
 var resultado = document.getElementById ('resultado');
+var texto = document.getElementById('txt');
+var base = document.getElementById('base');
+var cifra = document.getElementById('cifra');
 
-var labelCifra = document.querySelector("#incremento");
-var inputCifra = document.querySelector("#numeroIncremento");
+
+
+cod.addEventListener('click', function(){
+   botao.innerText='Codificar mensagem!'
+})
+
+decod.addEventListener('click', function(){
+   botao.innerText='Decodificar mensagem!'
+})
 
 seleciona.addEventListener('change', function(){
-    if(seleciona.value == 'cifraCesar'){
-        labelCifra.style.display = "block";
-        inputCifra.style.display = "block";
-    }else {
-        labelCifra.style.display = "none";
-        inputCifra.style.display = "none";
-    }
-});
+   if (selection.value === '0' || selection.value === 'base') {
+      document.getElementById('incremento').style.display = 'none';
+   } else {
+      document.getElementById('incremento').style.display = 'block'
+   } 
+})
+
+ botao.addEventListener('click', function(event){
+    event.preventDefault();
+    codifica(texto.value);
+ })
+
+
+
+
+
+///seleciona.addEventListener('change', function(){
+   // if (opcaocodigo.value === '0' || opcaocodigo.value === 'base64') {
+      // document.querySelector('.incremento').style.display  = 'none';
+    //} else {
+      // document.querySelector('.incremento').style.display = 'flex'
+   // }
+ //})
+
